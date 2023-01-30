99c3ea69fb8d40e092e7103e3df89743390cbe9a
Kayo.io <kayorenatocontato@gmail.com>
2023-01-30 01:31:42 -0300

    chore: adjust tailwind imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
-    <div>
+    <div className={
+      `
+      h-screen
+      flex
+      flex-col
+      justify-center
+      items-center
+      bg-gradient-to-r from-slate-900 to-slate-800
+      
+      `
+    }>
       <h1>Test</h1>
     </div>
   )
