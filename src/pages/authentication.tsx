cc6ca10e2fcb2ef22f4dc4eb58034a837e6065ac
Kayo.io <kayorenatocontato@gmail.com>
2023-02-02 01:09:27 -0300

    chore: update sign page without integration

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,7 +1,7 @@
 import { type } from "os";
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
-import { DarkIcon, GoogleLogo } from "../components/icons";
+import { GoogleLogo } from "../components/icons";
 
 export default function Authentication() {
 
@@ -13,38 +13,62 @@ export default function Authentication() {
 
     return (
         <div className={`
-            flex flex-col h-screen 
-            justify-center items-center
+            flex flex-row h-screen items-center justify-center
+    
         `}>
-            <div className={`
-                w-80
-            `}>
-                <h1 className={
-                    `
+            <div className={
+                `
+                hidden md:block md:w-1/2
+                lg:w-2/3
+
+                `
+
+            }>
+                <picture>
+                    <img
+                        src="https://source.unsplash.com/random"
+                        alt="random image of the authentication screen"
+                        className={`h-screen w-full object-cover`}
+                    />
+                </picture>
+
+            </div>
+            <div className={
+                `
+                flex justify-center items-center
+                lg:w-1/3
+                md:w-1/2
+                m-10
+
+                `
+            }>
+                <div className="w-80 md:w-96">
+                    <h1 className={
+                        `
                     text-2xl
                     font-semibold
                     text-center
                     mb-8
                     `
-                }>Login</h1>
-                <AuthInput
-                    label="Email"
-                    value={email}
-                    onChange={setEmail}
-                    type="email"
-                    placeholder="Insert your email"
-                    isRequired
-                />
-                <AuthInput
-                    label="Password"
-                    value={passwordInput}
-                    onChange={setPasswordInput}
-                    type="password"
-                    placeholder="Enter your password"
-                    isRequired
-                />
-                <button className={
-                    `
+                    }>Login</h1>
+                    <AuthInput
+                        label="Email"
+                        value={email}
+                        onChange={setEmail}
+                        type="email"
+                        placeholder="Insert your email"
+                        isRequired
+                    />
+                    <AuthInput
+                        label="Password"
+                        value={passwordInput}
+                        onChange={setPasswordInput}
+                        type="password"
+                        placeholder="Enter your password"
+                        isRequired
+                    />
+                    <button className={
+                        `
                     w-full
                     text-white
                     font-semibold
@@ -54,14 +78,14 @@ export default function Authentication() {
                     bg-blue-600
                     hover:bg-blue-500
                     `
-                }>
-                    Sign in
-                </button>
+                    }>
+                        Sign in
+                    </button>
 
-                <hr className={`my-6 border-gray-300 w-full`} />
+                    <hr className={`my-6 border-gray-300 w-full`} />
 
-                <button className={
-                    `
+                    <button className={
+                        `
                     flex flex-row
                     justify-center
                     items-center
@@ -69,14 +93,36 @@ export default function Authentication() {
                     text-white
                     font-semibold
                     mt-6
+                    mb-4
                     h-11
                     rounded-md
                     bg-red-600
                     hover:bg-red-500                   
                     `
-                }>
-                   {GoogleLogo} Sign in with Google
-                </button>
+                    }>
+                        {GoogleLogo} Sign in with Google
+                    </button>
+                    {mode === 'login' ? (
+                        <p>
+                            <a onClick={() => setMode("externAuth")} className={
+                                `
+                                text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                                `
+                            }> Sign up</a>
+                        </p>
+
+                    ) : (
+
+                        // eslint-disable-next-line react/no-unescaped-entities
+                        <p>Didn't you still register?
+                            <a onClick={() => setMode("externAuth")} className={
+                                `
+                                text-blue-500 hover:text-blue-700  cursor-pointer
+                                `
+                            }> Sign up now</a>
+                        </p>
+                    )}
+                </div>
             </div>
 
 
